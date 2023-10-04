8b0b1a23835e27c65e1acf852bc32c0ff864daf7
Dhruv Sharma <dsd71213@gmail.com>
2023-10-04 19:36:50 +0530

    Still Trying

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function sendEmail() {
+    sendEmail.send({
+        Host: "smtp.gmail.com",
+        Username: "dhrruvsharma@outlook.com",
+        Password: "dsd71214@gmail",
+        To: 'dsd71213@gmail.com',
+        From: document.getElementById("Email").value,
+        Subject: "Someone Contacted You",
+        Body: "Name: " + getElementById("Name").value
+            + "<br> Email: " + getElementById("Email").value
+            + "<br> Message: " + getElementById("message").value
+    }).then(
+        message => alert("Message sent successfully")
+    );
+}
